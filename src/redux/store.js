f41a9dc979a11e5eb3ce6a5da295269489fd0078
hotgorod <hotgorod@gmail.com>
2023-10-18 18:12:28 +0100

    enhancer added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createReducer } from '@reduxjs/toolkit';
 import contactReducer from './contactSlice';
+import { devToolsEnhancer } from '@redux-devtools/extension';
 
 
-const myReducer = createReducer()
+
+// const myReducer = createReducer()
+
+const enhancer = devToolsEnhancer();
+
 
 export const store = configureStore({
   reducer: {
     contacts: contactReducer,
   },
-});
+}, enhancer);

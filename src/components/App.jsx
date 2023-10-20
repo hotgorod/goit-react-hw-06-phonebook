07c9e522bdd412cb7b1ad1860dc6d70bb5de7f78
hotgorod <hotgorod@gmail.com>
2023-10-20 16:36:11 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from 'react';
 import React from 'react';
-// import { nanoid } from 'nanoid';
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
 import ContactForm from './ContactForm/ContactForm.';
@@ -8,11 +6,11 @@ import css from './App.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'redux/contactSlice';
 
-const LS_KONTACT_KEY = 'contact_detais';
+
 
 export const App = () => {
   
-  const [filter, setFilter] = useState('');
+ 
   const contacts = useSelector(state => state.contacts.contacts);
   const dispatch = useDispatch();
   const addContactToPhonebook = (contactName, contactNumber) => {
@@ -26,27 +24,7 @@ export const App = () => {
     }
     dispatch(addContact({contactName, contactNumber}));
   }
-  // useEffect(() => {
-  //   const jsonContacts = JSON.stringify(contacts);
-  //   localStorage.setItem(LS_KONTACT_KEY, jsonContacts);
-  // }, [contacts]);
-
-
-  // const changeFilter = event => {
-    
-  //   setFilter(event.currentTarget.value);
-  // };
-
-  // const getFilteredContacts = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-  //   return contacts.filter(contact =>
-  //     contact.contactName.toLowerCase().includes(normalizedFilter)
-  //   );
-  // };
-
-  // const filteredContacts = getFilteredContacts();
-
-  return (
+   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
       <ContactForm addContactCallback={addContactToPhonebook} />
